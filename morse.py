6a745ea0ae3cce018368ab5982baca443fbc2201
sgspectra <astorch13@gmail.com>
2014-12-31 00:11:36 -0500

    changed single to double quote in morseCode function

--- morse.py
+++ morse.py
@@ -23,110 +23,110 @@ def sht():
     time.sleep(0.25)
 	    
 def morseCode(x):
-    if x == 'a' or 'A':
+    if x == "a" or "A":
         sht()
         lng()
-    elif x == 'b' or 'B':
-        lng()
-        sht()
-        sht()
-        sht()
-    elif x == 'c' or 'C':
-        lng()
-        sht()
-        lng()
-        sht()
-    elif x == 'd' or 'D':
+    elif x == "b" or "B":
         lng()
         sht()
         sht()
-    elif x == 'e' or 'E':
         sht()
-    elif x == 'f' or 'F':
+    elif x == "c" or "C":
+        lng()
+        sht()
+        lng()
+        sht()
+    elif x == "d" or "D":
+        lng()
+        sht()
+        sht()
+    elif x == "e" or "E":
+        sht()
+    elif x == "f" or "F":
         sht()
         sht()
         lng()
         sht()
-    elif x == 'g' or 'G':
+    elif x == "g" or "G":
         lng()
         lng()
         sht()
-    elif x == 'h' or 'H':
+    elif x == "h" or "H":
         sht()
         sht()
         sht()
         sht()
-    elif x == 'i' or 'I':
+    elif x == "i" or "I":
         sht()
         sht()
-    elif x == 'j' or 'J':
-        sht()
-        lng()
-        lng()
-        lng()
-    elif x == 'k' or 'K':
-        lng()
-        sht()
-        lng()
-    elif x == 'l' or 'L':
-        sht()
-        lng()
-        sht()
-        sht()
-    elif x == 'm' or 'M':
-        lng()
-        lng()
-    elif x == 'n' or 'N':
-        lng()
-        sht()
-    elif x == 'o' or 'O':
-        lng()
-        lng()
-        lng()
-    elif x == 'p' or 'P':
+    elif x == "j" or "J":
         sht()
         lng()
         lng()
+        lng()
+    elif x == "k" or "K":
+        lng()
         sht()
-    elif x == 'q' or 'Q':
+        lng()
+    elif x == "l" or "L":
+        sht()
+        lng()
+        sht()
+        sht()
+    elif x == "m" or "M":
+        lng()
+        lng()
+    elif x == "n" or "N":
+        lng()
+        sht()
+    elif x == "o" or "O":
+        lng()
+        lng()
+        lng()
+    elif x == "p" or "P":
+        sht()
+        lng()
+        lng()
+        sht()
+    elif x == "q" or "Q":
         lng()
         lng()
         sht()
         lng()
-    elif x == 'r' or 'R':
+    elif x == "r" or "R":
         sht()
         lng()
         sht()
-    elif x == 's' or 'S':
+    elif x == "s" or "S":
         sht()
         sht()
         sht()
-    elif x == 't' or 'T':
+    elif x == "t" or "T":
         lng()
-    elif x == 'u' or 'U':
-        sht()
-        sht()
-        lng()
-    elif x == 'v' or 'V':
-        sht()
+    elif x == "u" or "U":
         sht()
         sht()
         lng()
-    elif x == 'w' or 'W':
+    elif x == "v" or "V":
         sht()
-        lng()
-        lng()
-    elif x == 'x' or 'X':
-        lng()
         sht()
         sht()
         lng()
-    elif x == 'y' or 'Y':
+    elif x == "w" or "W":
+        sht()
+        lng()
+        lng()
+    elif x == "x" or "X":
+        lng()
+        sht()
+        sht()
+        lng()
+    elif x == "y" or "Y":
         lng()
         sht()
         lng()
         lng()
-    elif x == 'z' or 'Z':
+    elif x == "z" or "Z":
         lng()
         lng()
         sht()
